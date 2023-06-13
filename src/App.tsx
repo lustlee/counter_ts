9fc5d61b34358d4405879afe5f4ca5c755ded555
Ramazan <r.tokoshev@gmail.com>
2023-06-13 19:41:56 +0600

    init and added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <h2>Counter:</h2>
+            <h1>0</h1>
+            <button className="decrement">- Decrement</button>
+            <button className="increment">Increment +</button>
+        </div>
     </div>
-  );
-}
+);
 
 export default App;
